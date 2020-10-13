6683d35fb69f5be31352885a49cdfad870841ea0
Edidiong <edidiong.aligbe@gmail.com>
2020-10-13 14:17:25 +0100

    update

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ app.post('/check', (req, res) =>{
     console.log (`${email}, ${password}, ${database}`)
 
     oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
-
-    const mypw = "utut"  // set mypw to the hr schema password
     
     async function run() {
     
@@ -30,7 +28,7 @@ app.post('/check', (req, res) =>{
         connection = await oracledb.getConnection( {
           user          : "Benjamin",
           password      : "Welcome@1234",
-          connectString : `(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.eu-frankfurt-1.oraclecloud.com))(connect_data=(service_name=erk8hib9rqpjwyi_rmar_high.atp.oraclecloud.com))(security=(ssl_server_cert_dn="CN=adwc.eucom-central-1.oraclecloud.com,OU=Oracle BMCS FRANKFURT,O=Oracle Corporation,L=Redwood City,ST=California,C=US")))`
+          connectString : "rmar_high"
         });
     
         const result = await connection.execute(
