317ea97990ef9ab03c8e42ab13e04b8175f8f52e
Edidiong <edidiong.aligbe@gmail.com>
2020-10-26 07:34:33 +0100

    update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var path = require("path");
 const session = require("express-session");
 const app = express();
 const oracledb = require("oracledb");
@@ -6,7 +7,8 @@ const oracledb = require("oracledb");
 port = process.env.PORT || 3000;
 
 //let sect = Math.random().toString(36).substr(2, 5);
-app.use(session({ resave: true, secret: "123456", saveUninitialized: true }));
+//app.use(session({ resave: true, secret: "123456", saveUninitialized: true }));
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
@@ -21,10 +23,12 @@ app.post("/check", (req, res) => {
 
   let duser = req.body.Username;
   let pass = req.body.Password;
-  //let db = req.body.Database;
-  // db.toUpperCase();
-  // let dbStr = `local/${db}`;
+  let db = req.body.Database;
+  // db = db.toUpperCase();
+  // let dbStr = `localhost/${db}`;
   let errS = null;
+
+  //console.log(dbStr);
 
   oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
@@ -41,7 +45,7 @@ app.post("/check", (req, res) => {
       console.log("Log in successfull");
     } catch (err) {
       console.error(err);
-      errS = err.toString();
+      errS = err.message;
       console.error(errS);
     } finally {
       if (connection) {
@@ -51,23 +55,23 @@ app.post("/check", (req, res) => {
           console.error(err);
         }
       }
-      if (errS === null) {
+      if (errS == null) {
         // Valid user
         res.render("pages/resetpassword.ejs");
-      } else if (errS === "Error: ORA-28000: The account is locked.") {
+      } else if (errS == 'ORA-28000: The account is locked.') {
         //Account locked
         res.render("pages/resetpassword.ejs");
       } else if (
-        errS === "ERROR: ORA-01017: invalid username/password; logon denied" ||
-        errS ===
-          "Error: ORA-12154: TNS:could not resolve the connect identifier specified"
+        errS == 'ORA-01017: invalid username/password; logon denied' ||
+        errS ==
+          'ORA-12154: TNS:could not resolve the connect identifier specified'
       ) {
         //Invalid login details
         res.render("pages/index.ejs", {
           loginDetails:
             "Wrong username or password has been supplied and password cannot be changed.",
         });
-      } else if (errS === "ORA-28001: the password has expired") {
+      } else if (errS == 'ORA-28001: the password has expired') {
         //Account expired
         res.render("pages/resetpassword.ejs");
       } else {
@@ -80,11 +84,24 @@ app.post("/check", (req, res) => {
   }
 
   run();
+
+ 
+
 });
 
 app.post("/fresh", (req, res) => {
   let fuser = req.body.Username;
   let fpass = req.body.Password;
+
+  let errS = null;
+  
+
+  // if (fpass !== fsecpass) {
+  //   res.render("pages/index.ejs", {
+  //     loginDetails:
+  //       "Wrong username or password has been supplied and password cannot be changed.",
+  //   });
+  // }
   
   let arr = [];
 
@@ -110,13 +127,10 @@ app.post("/fresh", (req, res) => {
         }
       );
   
-     arr = result.rows;
-     console.log(result.rows);
+    //  arr = result.rows;
+    //  console.log(result.rows);
 
-     res.render("pages/index.ejs", {
-      loginDetails:
-        "Password updates successfully.",
-    });
+    //  res.render("pages/passwordsuccess.ejs");
   
     //  console.log(result.rows);
     //  if (arr[0].DEFAULT_TABLESPACE !== "USERS") {
@@ -129,6 +143,7 @@ app.post("/fresh", (req, res) => {
     //       }
     } catch (err) {
       console.error(err);
+      errS = err.message;
     } finally {
       if (connection) {
         try {
@@ -136,6 +151,10 @@ app.post("/fresh", (req, res) => {
         } catch (err) {
           console.error(err);
         }
+      }
+      if (errS == 'ORA-28000: The account is locked.') {
+        //Account locked
+        res.render("pages/passwordsuccess.ejs");
       }
     }
   }
@@ -146,43 +165,8 @@ app.post("/fresh", (req, res) => {
 
 });
 
-app.all("/attempt", function (req, res) {
-  console.log("Attempting to connect user......");
-  let username = sess.Username;
-  let pass = sess.Password;
-  let db = sess.Database;
-  let dbStr = `localhost/${db}`;
-  let errNum = null;
 
-  console.log(pass);
-  //Attempt login
-  oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
-
-  async function run() {
-    let connection;
-
-   
-  }
-
-  run();
-
-  console.log(errNum);
-  if (errNum === 28000) {
-    //Account is locked
-    res.render("pages/resetpassword.js");
-  } else if (errNum === 01017) {
-    //Invalid login details
-    res.render("pages/index.ejs", {
-      loginDetails:
-        "Wrong username or password has been supplied and password cannot be changed.",
-    });
-  } else {
-    //Any other exception
-    res.render("pages/index.ejs", {
-      loginDetails: "Unable to carry out opereation, please try again later.",
-    });
-  }
-});
+ 
 
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`);
